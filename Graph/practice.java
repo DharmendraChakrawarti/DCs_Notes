2d0977b747417d2c08e8022fc13b5262bb2401a1
Dharmendra Chakrawarti <dharmendrachakrawarti9657@gmail.com>
2022-03-02 20:35:13 +0530

    Add files via upload

---
+++ Graph/practice.java
@@ -0,0 +1,10 @@
+public class practice {
+    public static void main(String[] args) {
+        int num=5;
+        int i=0;
+        do{
+        System.out.println(i);
+        i++;
+        }while(num>=i);
+    }
+}
